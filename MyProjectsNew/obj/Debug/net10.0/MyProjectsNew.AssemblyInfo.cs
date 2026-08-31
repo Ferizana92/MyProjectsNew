7a329c2e993a502e570e96719a5e7a11ed42d3c8
f.motevalian <f.motevalian@FW-10149>
2026-08-31 15:27:03 +0330

    First commit

--- MyProjectsNew/obj/Debug/net10.0/MyProjectsNew.AssemblyInfo.cs
+++ MyProjectsNew/obj/Debug/net10.0/MyProjectsNew.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProjectsNew")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92b84b014be462ce58623cc0a983c843b8ad1db1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProjectsNew")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProjectsNew")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
